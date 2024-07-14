4abb6c142799f526dd9fd0674b4650fdd9ce750a
Krishna <gkrrish.11@gmail.com>
2024-07-14 21:02:48 +0530

    initial commit

---
+++ src/main/java/com/advt/TargetAdvertisementApplication.java
@@ -0,0 +1,13 @@
+package com.advt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetAdvertisementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TargetAdvertisementApplication.class, args);
+	}
+
+}
