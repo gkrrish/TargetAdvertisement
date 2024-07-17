9dca5f68a7c6b642d93ee9efbf7c2c2f1a07d950
Krishna <gkrrish.11@gmail.com>
2024-07-17 17:35:11 +0530

    REST :: get STATE, DISTRICT, MANDAL from external services

--- src/main/java/com/advt/controller/AdvertisementUserController.java
+++ src/main/java/com/advt/controller/AdvertisementUserController.java
@@ -3,22 +3,28 @@ package com.advt.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.advt.entity.TargetedAdvertisementPlan;
 import com.advt.request.WelcomeRequest;
 import com.advt.response.AdvertiserResponse;
 import com.advt.service.AdvertisementUserService;
+import com.advt.service.ExternalServiceCalls;
 
 @RestController
 public class AdvertisementUserController {
 
 	@Autowired
 	private AdvertisementUserService advertisementUserService;
+	@Autowired
+	private ExternalServiceCalls externalServiceCalls;
+
 
 	@PostMapping("/welcome-advertiser")
 	public AdvertiserResponse handleHiMessage(@RequestBody WelcomeRequest welcomeRequest) {
@@ -26,9 +32,25 @@ public class AdvertisementUserController {
 	}
 	
 	
-	@GetMapping("/available-plans/{locationId}")
-    public List<TargetedAdvertisementPlan> getAvailablePlansByLocationId(@PathVariable Integer locationId) {
-        return advertisementUserService.getAvailablePlansByLocationId(locationId);
+	@GetMapping("/states/{mobileNumber}")
+    public List<String> getStates(@PathVariable String mobileNumber) {
+        return externalServiceCalls.getStates(mobileNumber);
     }
 
+    @GetMapping("/districts/{mobileNumber}/{stateName}")
+    public List<String> getDistricts(@PathVariable String mobileNumber, @PathVariable String stateName) {
+        return externalServiceCalls.getDistricts(mobileNumber, stateName);
+    }
+
+    @GetMapping("/mandals/{mobileNumber}/{districtName}")
+    public List<String> getMandals(@PathVariable String mobileNumber, @PathVariable String districtName) {
+        return externalServiceCalls.getMandals(mobileNumber, districtName);
+    }
+	
+
+	@GetMapping("/available-plans/{locationId}")
+	public List<TargetedAdvertisementPlan> getAvailablePlansByLocationId(@PathVariable Integer locationId) {
+		return advertisementUserService.getAvailablePlansByLocationId(locationId);
+	}
+	
 }
---
+++ src/main/java/com/advt/service/ExternalServiceCalls.java
@@ -0,0 +1,36 @@
+package com.advt.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ExternalServiceCalls {
+
+    private static final String BASE_URL = "http://localhost:8383/";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public List<String> getStates(String mobileNumber) {
+        String url = BASE_URL + "states/{mobileNumber}";
+        return restTemplate.exchange(url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<String>>() {}, mobileNumber).getBody();
+    }
+
+    public List<String> getDistricts(String mobileNumber, String stateName) {
+        String url = BASE_URL + "districts/{mobileNumber}/{stateName}";
+        return restTemplate.exchange(url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<String>>() {}, mobileNumber, stateName).getBody();
+    }
+
+    public List<String> getMandals(String mobileNumber, String districtName) {
+        String url = BASE_URL + "mandals/{mobileNumber}/{districtName}";
+        return restTemplate.exchange(url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<String>>() {}, mobileNumber, districtName).getBody();
+    }
+}
